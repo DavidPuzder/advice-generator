959b7b2646f6e640c376b170424fd2c887fc39f5
David Puzder <spam@davidpuzder.sk>
2022-11-27 19:58:48 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="id">
+     <div v-if="id">ADVICE  #{{ id }}</div>
+      <br>
+      <br>
+    </div>
+    <div class="advice">
+      <div v-if="advice">" {{ advice }} ,,</div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      advice: "",
+      id:""
+    }
+  },
+  mounted () {
+    this.getAdvice()
+  },
+  methods: {
+    async getAdvice() {
+      const res = await axios.get(`https://api.adviceslip.com/advice`);
+      this.advice = res.data.slip.advice
+      this.id = res.data.slip.id
+    },
+
   }
 }
 </script>
+
+<style >
+  .home{
+    background: #2c3e50;
+    color: aliceblue;
+    margin:  20%;
+    border-radius: .5em;
+    padding-bottom: 2em;
+    font-weight: 700;
+  }
+
+.id{
+  color: #42b983;
+  padding-top: 2em;
+  font-size: .8em;
+  letter-spacing: 2px;
+}
+
+.advice{
+  font-size: 1.4em;
+  font-weight: 800;
+
+}
+</style>
